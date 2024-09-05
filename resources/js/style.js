644f866b4dbf7d6d020754debea031fa356a1951
mjokarr <mo.jokar80@gmail.com>
2024-09-05 13:12:19 +0330

    home UI is comleted

--- resources/js/style.js
+++ resources/js/style.js
@@ -1,3 +1,9 @@
 $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
  });
+
+ $(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
